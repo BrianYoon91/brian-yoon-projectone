763ba67d2ab3de3b818986828e8f77c3f34ac69d
Brian Yoon <yoon.brian91@gmail.com>
2022-05-15 14:09:03 -0400

    added contact form feature and showing cursor on the buttons

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// Responsive burger menu for mobile
+// Responsive burger menu for mobile. The burger menu will popout if width < 1280px
 
 const burgerMenu = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
@@ -6,3 +6,25 @@ const menu = document.querySelector('.menu');
 burgerMenu.addEventListener('click', ()=>{ 
     menu.classList.toggle('show');
 })
+
+// Contact form popout after submission 
+
+const formSubmit = document.querySelector('.contactForm');
+
+formSubmit.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const userName = document.getElementById('name').value;
+    const userEmail = document.getElementById('email').value; 
+
+        if (userName === "" && userEmail  === "") {
+
+            alert('Please enter your name and email to contact us!');
+
+        } else {
+
+            alert(`Hey ${userName} we will follow up with you at ${userEmail} as soon as we can! Thank you for your patience!`);
+
+        }
+
+});
